8db8e45ef9ba2b3765d245c7005d99b232d7e8da
lixiang1991 <493081998@qq.com>
2018-01-24 13:47:07 +0800

    9

--- typewriter.js
+++ typewriter.js
@@ -38,7 +38,7 @@ Element.prototype.typewriter=function(a){
 			  ,btn: ['喜欢', '不喜欢']
 			  ,yes: function(index){
 			    layer.close(index);
-			    like();
+			    like(0);
 			  }
 			  ,btn2:function(index){
 			 	layer.close(index);
@@ -56,7 +56,7 @@ Element.prototype.typewriter=function(a){
 			  }
 			  ,btn2:function(index){
 			 	layer.close(index);
-			   	like();
+			   	like(0);
 			  }
 			});
 		}
@@ -66,17 +66,17 @@ Element.prototype.typewriter=function(a){
 			  ,btn: ['yes', 'no']
 			  ,yes: function(index){
 			    layer.close(index);
-			    like();
+			    like(0);
 			  }
 			  ,btn2:function(index){
 			 	layer.close(index);
-			   	like();
+			   	like(0);
 			  }
 			});
 		}
 	}
 	
-	 function like(){
+	 function like(no_count){
 	 	var no_text='不好';
 	 	if(dislike_count>0){
 	 		no_text='想得美';
@@ -84,7 +84,11 @@ Element.prototype.typewriter=function(a){
 	 	if(dislike_count==3){
 	 		no_text='不要脸';
 	 	}
-	 	layer.msg('我也喜欢你，做我女票好吗 O.o', {
+	 	var title_head='我也喜欢你';
+	 	if(no_count>0){
+	 		title_head='再问你一遍';
+	 	}
+	 	layer.msg(title_head+'，做我女票好吗 O.o', {
 		      time: 0 //不自动关闭
 		      ,btn: ['好呀',no_text]
 		      ,yes: function(index){
@@ -95,9 +99,9 @@ Element.prototype.typewriter=function(a){
 			 	layer.close(index);
 			 	if(dislike_count==0)
 			   		nogf();
-			   	if(dislike_count>0 && dislike_count<3)
+			   	else if(dislike_count==1 || dislike_count==2)
 			   		think();
-			   	if(dislike_count==3)
+			   	else if(dislike_count==3)
 			   		noface();
 			  }
 		    });
@@ -105,8 +109,44 @@ Element.prototype.typewriter=function(a){
 	 function think(){
 	 	var index = layer.open({
 	 		closeBtn:0,
-		  	content: $("#sleep"),
-		  	title:'我能想到最美的事就是每天醒来第一眼就能见到你',
+		  	content: '<h3>每天醒来第一眼就能见到你</h3><img id="sleep" src="img/sleep.jpg" width="200px" ></img>',
+		  	title:'我能想到最美的事',
+		  	btn:['重新选择'],
+		  	yes:function(index,layero){
+		  		layer.close(index);
+		  		restart();
+		  	}
+		});
+	 }
+	  function noface(){
+	 	var index = layer.open({
+	 		closeBtn:0,
+		  	content: '<h3>凭此截图找[糕～]兑换dior润唇膏</h3><img id="sleep" src="img/hong.jpg" width="150px" ></img>',
+		  	title:'令人窒息的美',
+		  	btn:['重新选择'],
+		  	yes:function(index,layero){
+		  		layer.close(index);
+		  		restart();
+		  	}
+		});
+	 }
+	  function love(){
+	 	var index = layer.open({
+	 		closeBtn:0,
+		  	content: '<h4>凭此截图找[糕～]兑换520红包</h4><img id="sleep" src="img/red.jpg" width="150px" ></img>',
+		  	title:'终于等到你',
+		  	btn:['重新选择'],
+		  	yes:function(index,layero){
+		  		layer.close(index);
+		  		restart();
+		  	}
+		});
+	 }
+	  function early(){
+	 	var index = layer.open({
+	 		closeBtn:0,
+		  	content: '<h4>凭此截图找[糕～]兑换零食大礼包</h4><img id="sleep" src="img/lingshi.jpg" width="150px" ></img>',
+		  	title:'等你哦',
 		  	btn:['重新选择'],
 		  	yes:function(index,layero){
 		  		layer.close(index);
@@ -148,7 +188,7 @@ Element.prototype.typewriter=function(a){
 			  icon: 2,
 			  time: 1000 
 			}, function(){
-			  startQue();
+			  like(1);
 			}); 
 	 }
  	function gf(){
